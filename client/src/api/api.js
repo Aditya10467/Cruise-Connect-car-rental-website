bc1730c9bd1d02bab1bf1f8e3c2f9a8c156ffc00
ADITYA TRIPATHY <adityatripathy1968@gmail.com>
2024-04-30 00:19:22 +0530

    update

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'https://cruise-connect-car-rental-website.onrender.com',
+  baseURL: 'https://cruise-connect-car-rental-website.onrender.com'
 })
 
 export default API
